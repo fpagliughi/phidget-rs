5fc43fb9a51eba24ab624d26f652d4e951c7e553
fpagliughi <fpagliughi@mindspring.com>
2023-08-15 21:39:47 -0400

    Added offset and gain options to the voltage_in example

--- examples/voltage_in.rs
+++ examples/voltage_in.rs
@@ -11,6 +11,26 @@
 //
 
 //! Rust Phidget example application to read voltage input values.
+//!
+//! A number of sensors have a simple, linear, formula to get a value in
+//! the form:
+//!
+//! ```text
+//! value = (voltage - offset) * gain
+//! ```
+//! This example app allows you to enter the gain and offset to see the value
+//! in the units of the sensor. For example, the [VCP4114.0 Clip-On Current
+//! Transducer](https://www.phidgets.com/?&prodid=1184) has the formula:
+//!
+//! ```text
+//! DC Amps = (V - 2.5) * 16.0
+//! ```
+//!
+//! So, to see the output in amps, run this:
+//!
+//! ```text
+//! $ voltage_in -o 2.5 -g 16.0
+//! ```
 
 use clap::{arg, value_parser, ArgAction};
 use phidget::Phidget;
@@ -28,7 +48,7 @@ fn main() -> anyhow::Result<()> {
     let opts = clap::Command::new("voltage_in")
         .version(VERSION)
         .author(env!("CARGO_PKG_AUTHORS"))
-        .about("Phidget Analog Input Example")
+        .about("Phidget Voltage (Analog) Input Example")
         .disable_help_flag(true)
         .arg(
             arg!(--help "Print help information")
@@ -48,6 +68,16 @@ fn main() -> anyhow::Result<()> {
                 .value_parser(value_parser!(i32)),
         )
         .arg(arg!(-h --hub "Use a hub VINT input port directly").action(ArgAction::SetTrue))
+        .arg(
+            arg!(-o --offset [offset] "The offset for reading  [val = gain * (volts - offset)]")
+                .default_value("0.0")
+                .value_parser(value_parser!(f64))
+        )
+        .arg(
+            arg!(-g --gain [gain] "The gain for the reading [val = gain * (volts - offset)]")
+                .default_value("1.0")
+                .value_parser(value_parser!(f64))
+        )
         .get_matches();
 
     let use_hub = opts.get_flag("hub");
@@ -71,6 +101,9 @@ fn main() -> anyhow::Result<()> {
         vin.set_channel(chan)?;
     }
 
+    let offset = *opts.get_one::<f64>("offset").unwrap();
+    let gain = *opts.get_one::<f64>("gain").unwrap();
+
     vin.open_wait(TIMEOUT)?;
 
     if use_hub {
@@ -79,10 +112,12 @@ fn main() -> anyhow::Result<()> {
     }
 
     let v = vin.voltage()?;
-    println!("Voltage: {}", v);
+    let val = (v - offset) * gain;
+    println!("{:.4}", val);
 
-    vin.set_on_voltage_change_handler(|_, v: f64| {
-        println!("Voltage: {}", v);
+    vin.set_on_voltage_change_handler(move |_, v: f64| {
+        let val = (v - offset) * gain;
+        println!("{:.4}", val);
     })?;
 
     // ^C handler wakes up the main thread
